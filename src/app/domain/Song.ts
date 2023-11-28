dbd1ab91a319114cfe5439c45f02fb8621d35536
vinayA1996 <126498393+vinayA1996@users.noreply.github.com>
2023-11-28 18:51:06 +0530

    till domain

---
+++ src/app/domain/Song.ts
@@ -0,0 +1,34 @@
+export interface SONG {
+  ID: string;
+  name?: string;
+  images?: string[];
+  lyrics?: string;
+  releaceDate?: string;
+  link?: string;
+  duration: number;
+  status?: Status;
+  writers?: Person[];
+  directors?: Person[];
+  composers?: Person[];
+  actors?: Person[];
+  singers?: Person[];
+}
+interface Person {
+  // Define properties for the Person type
+}
+
+enum Status {}
+// Define possible status values
+export const songs: SONG[] = [
+  {
+    ID: '1',
+    name: 'Imaan Tu',
+    images: ['image1', 'image2'],
+    lyrics: 'Surveen Kaur',
+    releaceDate: 'xx',
+    duration: 4.57,
+    directors: [{ name: 'Akshay K Agarwal' }],
+    composers: [{ name: 'Amir Khan' }],
+    singers: [{ name: 'Ankit Tiwari' }],
+  },
+];
---
+++ src/app/home/landingpage/song-layout1/song-layout1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-layout1',
+  templateUrl: './song-layout1.component.html',
+  styleUrls: ['./song-layout1.component.scss']
+})
+export class SongLayout1Component {
+
+}
