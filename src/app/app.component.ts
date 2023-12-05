159d303e4a9e57a053eefd59501e1171d5a1f17e
vinayA1996 <126498393+vinayA1996@users.noreply.github.com>
2023-12-05 13:46:39 +0530

    updated ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gkmusic';
+  constructor(private meta: Meta) {}
+  ngOnInit() {
+    this.meta.addTag({ name: 'description', content: 'Your page description' });
+    // Add other meta tags as needed
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     HomeModule,
     CommonComponentsModule,
